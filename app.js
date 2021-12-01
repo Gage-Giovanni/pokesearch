646f0c544c0b8486b5c7c23ea820bd3e07005222
Gage Giovanni <90288516+Gage-Giovanni@users.noreply.github.com>
2021-11-30 18:31:32 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+const pokemonList = document.getElementById('pokemonList');
+const searchBox = document.getElementById('searchBox');
+let loadScreen = document.getElementById('loadContainer');
+
+let pokeList = [];
+let pokeResults = [];
+
+// listen for keypresses on the filter bar, and update the list of returned
+// pokemon based on the input
+searchBox.addEventListener('keyup', (input) => {
+    inputStr = input.target.value;
+    pokeResults = pokeList.filter(pokemon => {
+        let validResult = false;
+        if(pokemon.name.toLowerCase().includes(inputStr.toLowerCase())) {
+            return true;
+        } else {
+            for(let i=0;i<pokemon.types.length;i++) {
+                if (pokemon.types[i].type.name.includes(inputStr)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    })
+    showCards(pokeResults);
+});
+
+// populate an array of all pokemon through api calls
+const fillPokeList = async () => {
+    try {
+        let count = 0;
+        for (let i = 1; i < 899; i++) {
+            const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+            pokeList[i] = await res.json();
+        }
+        loadScreen.style.display = "none";
+        showCards(pokeList);
+    } catch(err) {
+        console.error(err);
+    }
+};
+
+// display the pokemon as cards on the website
+const showCards = (pokemons) => {
+    const htmlString = pokemons
+        .map((pokemon) => {
+            return `
+                <li class="pokemon">
+                    <img src="${pokemon.sprites.front_default}"></img>
+                    <h2>${pokemon.name}</h2>
+                    <p>#${pokemon.id}</p>
+                    <p>Type: ${pokemon.types[0].type.name}</p>
+                </li>
+            `;
+        })
+        .join('');
+    pokemonList.innerHTML = htmlString;
+};
+
+fillPokeList();
